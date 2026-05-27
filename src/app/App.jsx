09e7976f54372e786d4fa132f22d7b3e8008a4c6
Amirreza Khalili <amirreza1384khalili@gmail.com>
2026-05-27 19:32:05 +0330

    style(Button):py changed

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,12 @@
 import '../assets/styles/App.css'
+import Button from '../components/atoms/Butoon/Button'
 import Logo from '../components/atoms/Logo/Logo'
 
 function App() {
 
   return (
     <>
-      
+      <Button children={"شروع یادگیری"} onClick={()=>{""}} />
     </>
   )
 }

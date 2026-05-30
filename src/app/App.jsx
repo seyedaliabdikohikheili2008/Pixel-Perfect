93c49930dcf5cd54a5380a67405899f56caf1549
kuooka <fatemehlohrasebi83@gmail.com>
2026-05-30 16:55:24 +0330

    feat(register):register form style added

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,7 +2,7 @@ import '../assets/styles/App.css'
 import Header from '../components/molecules/header/Header'
 import Footer from '../components/organisms/Footer/Footer'
 import LoginStepOneForm from '../components/organisms/loginStepOneForm/LoginStepOneForm'
-import LoginStepOne from '../components/templates/login/loginStepOne/LoginStepOne'
+import RegisterStepOne from "../components/organisms/RegisterStepOne/RegisterStepOne"
 
 
 
@@ -14,7 +14,7 @@ function App() {
       <div className='flex justify-center flex-col '>
         <Header variant={"linear"} />
         {/* <LoginStepOne/> */}
-        <LoginStepOneForm/>
+        <RegisterStepOne/>
         <Footer/>
       </div>
 
---
+++ src/components/organisms/RegisterStepOne/RegisterStepOne.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import Logo from "../../atoms/Logo/Logo";
+import { Form, Formik,  ErrorMessage } from "formik";
+import Button from "../../atoms/Butoon/Button";
+import Input from "../../atoms/Input/Input";
+import phone from "../../../assets/images/icons/login-signup-form/phone.png";
+import * as Yup from "yup";
+const registerStepOneForm = () => {
+const validationSchema=Yup.object({
+  email: Yup.string().required("ایمیل الزامی است"),
+})
+  return (
+    <div className="w-full">
+      <div className=" w-full rounded-xl bg-white md:w-1/3 mx-auto flex flex-col items-center gap-6 py-8 dark:bg-dark-bg-2">
+        <Logo variant={"vertical"} className={"h-52.25"} />
+        <h1 className="font-bold font-sans text-textC dark:text-white text-3xl">
+          ایجاد حساب کاربری
+        </h1>
+        <p className="font-normal text-xl">
+          لطفا شماره موبایل یا ایمیل خود را وارد کنید
+        </p>
+        <Formik
+          initialValues={{
+            email: ""
+          }}
+          onSubmit={(values) => {
+            console.log("Submit:", values);
+          }}
+          validationSchema={validationSchema}
+        >
+          {({ values, handleChange, handleSubmit,handleBlur,errors={}, touched }) => (
+            <Form
+              className="w-full flex flex-col items-center gap-6"
+              onSubmit={handleSubmit}
+            >
+                <div className="w-8/10 flex flex-col justify-end">
+              <div className={` focus:ring-indigo-500 w-full bg-neutral-50 dark:bg-dark-bg-3 rounded-xl flex flex-col justify-end focus:border-indigo-500  ${
+                errors.email && touched.email ? ' border border-red-500' : ''
+              }`}>
+                <Input
+                  icon={phone}
+                  placeholder={"ایمیل یا شماره تماس"}
+                  iconClassname={"mx-3.5"}
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  handleBlur={handleBlur}
+                />
+              </div>
+              <ErrorMessage className="text-danger-500 text-right" name="email" component={"span"}/>
+              </div>
+              
+              <Button
+                children={"ارسال کد یکبار مصرف"}
+                buttonClassName="w-8/10 font-[18px]"
+                type="submit"
+              />
+            </Form>
+          )}
+        </Formik>
+
+        <div className="flex items-center gap-1">
+          <p className="">حساب کاربری دارید؟</p>
+          <p className="text-[#0CBDE2] ">وارد شوید</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default registerStepOneForm;

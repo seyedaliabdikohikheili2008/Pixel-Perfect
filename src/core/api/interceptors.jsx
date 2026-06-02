a6429e3ce1524722bad0f5d83230690f792718ca
Amirreza Khalili <amirreza1384khalili@gmail.com>
2026-06-02 13:55:01 +0330

    chor(axios): interceptor created.

---
+++ src/core/api/interceptors.jsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_BASE_URL = "http://188.121.111.8:3001";
+
+const ApiClient = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+ApiClient.interceptors.response.use((res) => {
+  return res.data;
+});
+
+export default ApiClient;

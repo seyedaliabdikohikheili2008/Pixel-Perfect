7f7198e10131b74e59703fe0836ee62aa1bf8612
Amirreza Khalili <amirreza1384khalili@gmail.com>
2026-06-02 17:28:19 +0330

    feat(landing): send data to newCourse card.

--- src/components/organisms/landing/new-course-slider/NewCourseSlider.jsx
+++ src/components/organisms/landing/new-course-slider/NewCourseSlider.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import NewCourseCard from "./NewCourseCard";
 import { getAllCourse } from "../../../../core/services/landing/getAllCourse";
 import { useAllCourses } from "../../../../core/hooks/queries/courses/useAllCoures";
 
 const NewCourseSlider = () => {
-  const [sliderStep, setsliderStep] = useState(1);
+  const [sliderStep, setsliderStep] = useState(0);
 
   const [params, setparams] = useState({
     PageNumber: 1,
@@ -12,19 +12,31 @@ const NewCourseSlider = () => {
   });
 
   const {
-    data: courseList = [],
+    data: courseList = undefined,
     isError: courseListErr,
     isLoading: courseListLoading,
   } = useAllCourses(params);
 
+  const newCourses = useMemo(() => {
+    if (courseList) {
+      return [...courseList.data.courseFilterDtos]
+        .sort((a, b) => new Date(b.startTime) - new Date(a.startTime))
+        .slice(0, 3);
+    }
+  }, [courseList]);
+
   return (
     <>
       <div className="flex flex-col items-center">
-        {courseList ? console.log("courses ", courseList) : ""}
-        {sliderStep == 1 ? <NewCourseCard /> : ""}
-        {sliderStep == 2 ? <NewCourseCard /> : ""}
-        {sliderStep == 3 ? <NewCourseCard /> : ""}
+        {/* {courseList ? console.log("courses ", newCourses) : ""} */}
+        <NewCourseCard detail={newCourses?.[sliderStep]} />
         <div className="flex gap-2.5">
+          <div
+            className={`${sliderStep == 0 ? "w-8 bg-primary-500" : "w-3 bg-neutral-300"} h-3 transition-[color,width] duration-500 rounded-full`}
+            onClick={() => {
+              setsliderStep(0);
+            }}
+          ></div>
           <div
             className={`${sliderStep == 1 ? "w-8 bg-primary-500" : "w-3 bg-neutral-300"} h-3 transition-[color,width] duration-500 rounded-full`}
             onClick={() => {
@@ -35,12 +47,6 @@ const NewCourseSlider = () => {
             className={`${sliderStep == 2 ? "w-8 bg-primary-500" : "w-3 bg-neutral-300"} h-3 transition-[color,width] duration-500 rounded-full`}
             onClick={() => {
               setsliderStep(2);
-            }}
-          ></div>
-          <div
-            className={`${sliderStep == 3 ? "w-8 bg-primary-500" : "w-3 bg-neutral-300"} h-3 transition-[color,width] duration-500 rounded-full`}
-            onClick={() => {
-              setsliderStep(3);
             }}
           ></div>
         </div>

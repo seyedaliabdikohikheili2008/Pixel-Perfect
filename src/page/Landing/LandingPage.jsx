8a641f97e8b8f626f30c80b4d9d7af4c44b8a91d
Amirreza Khalili <amirreza1384khalili@gmail.com>
2026-06-04 16:04:05 +0330

    refactor(Structure): folder edited.

---
+++ src/page/Landing/LandingPage.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Header from '../../components/organisms/header/Header'
+import Footer from '../../components/organisms/Footer/Footer'
+import LandingHeroSection from '../../components/templates/landing/LandingHeroSection'
+import NewCourse from '../../components/templates/landing/NewCourse'
+import LandingBestTeacher from '../../components/templates/landing/LandingBestTeacher'
+import LandingAboutMe from '../../components/templates/landing/LandingAboutMe'
+import Roadmap from '../../components/templates/landing/RoadMap'
+import LandingBestCourse from '../../components/templates/landing/LandingBestCourse'
+import PopularCtegory from '../../components/templates/landing/PopularCtegory'
+import LandingNewNews from '../../components/templates/landing/LandingNewNews'
+
+const LandingPage = () => {
+  return (
+    <>
+        <LandingHeroSection />
+        <NewCourse />
+        <LandingBestTeacher />
+        <LandingAboutMe />
+        <Roadmap />
+        <LandingBestCourse />
+        <PopularCtegory />
+
+        <LandingNewNews />
+    </>
+  )
+}
+
+export default LandingPage

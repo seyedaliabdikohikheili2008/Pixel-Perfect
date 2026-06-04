9f110d94cd4e4845478a6ee09929158657f0248a
Amirreza Khalili <amirreza1384khalili@gmail.com>
2026-06-04 18:00:58 +0330

    feat(course-list): course list view created.

--- src/components/organisms/course-list/course-card/CourseCard.jsx
+++ src/components/organisms/course-list/course-card/CourseCard.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import Img from "../../../../assets/images/courses/course-card/38bdd9e8afe89a72aeaf82a4cb63301c8ca9bb7b.jpg";
 import star from "../../../../assets/images/icons/courses/star.png";
 import Button from "../../../atoms/Butoon/Button";
 import teaching from "../../../../assets/images/icons/courses/teaching.png";
 import student from "../../../../assets/images/icons/landing/student-card.png";
-const CourseCard = () => {
+const CourseCard = ({ cardView2 }) => {
   const renderStars = (count) => {
     if (0 <= count <= 5) {
       return Array.from({ length: count }, (item, index) => (
@@ -13,31 +13,26 @@ const CourseCard = () => {
     }
   };
 
-  const [view2, setview2] = useState(true);
-
   return (
     <>
       <div
-        onClick={() => {
-          setview2((prev) => !prev);
-        }}
-        className={`${view2 ? "w-full flex-row items-center p-4 gap-5 shadow-2xl h-75 bg-background rounded-4xl" : "w-70 flex-col"}  flex `}
+        className={`${cardView2 ? "w-full flex-row items-center p-4 gap-5 shadow-2xl h-75 bg-background rounded-4xl" : "w-70 flex-col"}  flex `}
       >
         <img
-          className={`${view2 ? "w-2/5 h-5/6 rounded-[100px]" : "w-full h-60 rounded-t-[20px]"}  overflow-hidden object-cover object-center`}
+          className={`${cardView2 ? "w-2/5 h-5/6 rounded-[100px]" : "w-full h-60 rounded-t-[20px]"}  overflow-hidden object-cover object-center`}
           src={Img}
           alt=""
         />
         <div
-          className={`${view2 ? "w-3/5 h-full" : "w-full h-55.5  bg-background shadow-2xl -translate-y-10"} flex flex-col justify-between px-3 py-6 rounded-2xl`}
+          className={`${cardView2 ? "w-3/5 h-full" : "w-full h-55.5  bg-background shadow-2xl -translate-y-10"} flex flex-col justify-between px-3 py-6 rounded-2xl`}
         >
           <h2
-            className={`${view2 ? "text-xl" : "text-base"} text-textC font-bold text-right`}
+            className={`${cardView2 ? "text-xl" : "text-base"} text-textC font-bold text-right`}
           >
             دوره آموزش جامع HTML5
           </h2>
           <p
-            className={`${view2 ? "line-clamp-4" : "line-clamp-2"} text-sm font-normal text-textC text-right`}
+            className={`${cardView2 ? "line-clamp-4" : "line-clamp-2"} text-sm font-normal text-textC text-right`}
           >
             Node.js یک پلتفرم قدرتمند برای توسعهٔ برنامههای سرور با استفاده از
             جاوااسکریپت است. با استفاده از Node.js، میتوانید اپلیکیشنهای سریع و

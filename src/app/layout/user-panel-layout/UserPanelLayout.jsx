c4dc1de4d01b8a1a54693ab5d1fe89d23ca8430a
Amirreza Khalili <amirreza1384khalili@gmail.com>
2026-06-20 11:51:35 +0330

    feat(user-panel): logout completed.

--- src/app/layout/user-panel-layout/UserPanelLayout.jsx
+++ src/app/layout/user-panel-layout/UserPanelLayout.jsx
@@ -21,7 +21,7 @@ const UserPanelLayout = () => {
       <Toaster />
       <div className="w-full relative overflow-x-hidden bg-dashboardBg flex items-start gap-5 p-4">
         <div
-          className={`lg:w-73 w-11/12 absolute ${menuFlag ? "flex" : "hidden"} top-24 lg:top-0 left-1/2 lg:left-0 -translate-x-1/2 lg:translate-x-0 lg:relative bg-background lg:flex flex-col gap-10 z-40 items-center h-160 py-6 rounded-3xl shadow-[0px_50px_100px_0px_#48484829]`}
+          className={`lg:w-73 w-11/12 absolute ${menuFlag ? "flex" : "hidden"} top-24 lg:top-0 left-1/2 lg:left-0 -translate-x-1/2 lg:translate-x-0 lg:relative bg-background lg:flex flex-col gap-10 z-40 items-center h-177 py-6 rounded-3xl shadow-[0px_50px_100px_0px_#48484829]`}
         >
           <img
             onClick={() => {
@@ -96,7 +96,13 @@ const UserPanelLayout = () => {
               <h3 className="text-lg">پروفایل</h3>
             </li>
           </ul>
-          <div className="lg:absolute cursor-pointer lg:bottom-5 gap-4 text-danger-500 text-lg rounded-[38px] items-center justify-center w-50 py-3 flex border border-neutral-200">
+          <div
+            onClick={() => {
+              localStorage.removeItem("token");
+              navigate("/");
+            }}
+            className="lg:absolute cursor-pointer lg:bottom-5 gap-4 text-danger-500 text-lg rounded-[38px] items-center justify-center w-50 py-3 flex border border-neutral-200"
+          >
             <IoLogOutOutline size={24} />
             <h3>خروج از حساب</h3>
           </div>

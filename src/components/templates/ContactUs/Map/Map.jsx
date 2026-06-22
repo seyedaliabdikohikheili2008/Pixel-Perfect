59b82467c08be58302f24d1e6889a63e0701faab
kuooka <fatemehlohrasebi83@gmail.com>
2026-06-22 23:13:11 +0330

    feat(contact-us):add mark in map

--- src/components/templates/ContactUs/Map/Map.jsx
+++ src/components/templates/ContactUs/Map/Map.jsx
@@ -1,18 +1,29 @@
 import React from "react";
-import telephon from "../../../../assets//images/contactUs/telephon.png";
-import telegram from "../../../../assets//images/contactUs/telegram.png";
-import instagram from "../../../../assets//images/contactUs/instagram.png";
-import web from "../../../../assets//images/contactUs/web.png";
-import { MapContainer, TileLayer } from "react-leaflet";
+import telephon from "../../../../assets/images/contactUs/telephon.png";
+import telegram from "../../../../assets/images/contactUs/telegram.png";
+import instagram from "../../../../assets/images/contactUs/instagram.png";
+import web from "../../../../assets/images/contactUs/web.png";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png",
+  iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png",
+  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
+});
+
 const Map = () => {
+  const position = [36.59729612973962, 53.06460213556759];
+
   return (
     <div className="w-11/12 p-5 m-auto flex flex-col md:flex-row gap-10 mb-10">
-      <div className=" w-full xl:w-1/4 bg-primary-200 p-5 rounded-xl flex flex-col gap-5">
+      <div className="w-full xl:w-1/4 bg-primary-200 p-5 rounded-xl flex flex-col gap-5">
         <div className="flex items-center bg-background p-1.5 rounded-lg gap-3">
           <img src={web} alt="" />
           <div className="text-right">
-            <p className="font-bold text-lg text-textC">ادرسه سایت</p>
+            <p className="font-bold text-lg text-textC">ادرس سایت</p>
             <p className="font-bold text-sm text-neutral-300">WWW.BaHr-AC.iR</p>
           </div>
         </div>
@@ -45,11 +56,16 @@ const Map = () => {
       <div className="xl:w-2/3 w-full h-85.5 rounded-xl">
         <MapContainer
           className="z-10"
-          center={[36.59729612973962, 53.06460213556759]}
+          center={position}
           zoom={15}
           style={{ height: "100%", width: "100%", borderRadius: "20px" }}
         >
           <TileLayer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <Marker position={position}>
+            <Popup>
+              موقعیت ما 📍
+            </Popup>
+          </Marker>
         </MapContainer>
       </div>
     </div>
